8fe2ece59a7e2f2d18d5a0b828b169391817d9f2
Mantas Ambroza <mantas.ambroza@gmail.com>
2024-06-16 12:22:57 +0200

    Added comments, updated README, fixed plot legend.

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# Testing README file
+# Laser Data Plotter
 
-## Add details here later
+The Laser Data Plotter is a project for the Codeacademy.lt beginners course in Python programming. The program loads
+data from a database file (.db) and plots a graph for a chosen parameter. The selected serial number is highlighted.
+Additionally, some basic statistics are calculated: standard deviation, mean, minimum, and maximum values.
+
+## Set Up
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install required packages.
+
+```bash
+pip install requirements.txt
+```
+
+Database file must be placed in the project folder.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+## License
+
+Copyright (c) 2024 Mantas Ambroza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import customtkinter as ctk
 import pandas as pd
 from sqlalchemy import create_engine
+from matplotlib.lines import Line2D
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import numpy as np
-from PIL import Image, ImageTk
+from PIL import Image
 
-engine = create_engine('sqlite:///lazeriu_duomenys.db')
+# Connect to the database and load data into pandas datafram
+engine = create_engine('sqlite:///database.db')
 df = pd.read_sql_table('lazeriu_duomenys', engine)
 df['Date'] = pd.to_datetime(df['Date'])
 
@@ -67,17 +69,82 @@ class PlotFrame(ctk.CTkFrame):
         self.toolbar.update()
         self.toolbar.pack(side='top', anchor='nw', fill='x')
 
-        print("PlotFrame initialized successfully")
-
 
 class PlotUpdateFrame(ctk.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, plot_frame):
         super().__init__(master)
+        self.plot_frame = plot_frame
+
+        # Create and configure check boxes
+        self.check_mean_var = ctk.StringVar(self, value='off')
+        self.check_mean = ctk.CTkCheckBox(self, text='Add Mean',
+                                          variable=self.check_mean_var,
+                                          onvalue='on',
+                                          offvalue='off')
+
+        self.check_std_var = ctk.StringVar(value='off')
+        self.check_std = ctk.CTkCheckBox(self, text='Standard Deviation (1σ)',
+                                         variable=self.check_std_var,
+                                         onvalue='on',
+                                         offvalue='off')
+
+        self.check_2std_var = ctk.StringVar(value='off')
+        self.check_2std = ctk.CTkCheckBox(self, text='Standard Deviation (2σ)',
+                                          variable=self.check_2std_var,
+                                          onvalue='on',
+                                          offvalue='off')
+
+        self.button_update = ctk.CTkButton(self, text='Update plot', command=self.update_plot)
+
+        # Grid checkboxes and button to the frame
+        self.check_mean.grid(row=0, column=1, pady=10, padx=15)
+        self.check_std.grid(row=0, column=2, pady=10, padx=15)
+        self.check_2std.grid(row=0, column=3, pady=10, padx=15)
+        self.button_update.grid(row=0, column=4, pady=10, padx=15)
+
+    def update_plot(self):
+        # Get required values
+        serial = self.master.entry_sn.get()
+        chosen_parameter = self.master.option_menu.get()
+        stdd = df[chosen_parameter].std()
+        mean = df[chosen_parameter].mean()
+
+        # Get values from the checkboxes
+        std_tick = self.check_std.get()
+        std2_tick = self.check_2std.get()
+        mean_tick = self.check_mean.get()
+
+        # Plot selected parameters for the ticked checkboxes
+        if mean_tick == 'on':
+            self.plot_frame.ax.axhline(y=mean, color='r', label='Mean')
+        if std_tick == 'on':
+            self.plot_frame.ax.axhspan(mean - stdd, mean + stdd, facecolor='b', alpha=0.25,
+                                       label='Standard Deviation (1σ)')
+        if std2_tick == 'on':
+            self.plot_frame.ax.axhspan(mean - stdd * 2, mean + stdd * 2, facecolor='b', alpha=0.15,
+                                       label='Standard Deviation (2σ)')
+
+        # Generate a new legend, with a manually marked serial number (same as in the main app)
+        handles, labels = self.plot_frame.ax.get_legend_handles_labels()
+        selected_laser_point = Line2D(
+            [0], [0],
+            label=serial,
+            marker='o',
+            markersize=10,
+            markerfacecolor='r',
+            markeredgecolor='r',
+            linestyle=''
+        )
+        handles.append(selected_laser_point)
+        self.plot_frame.ax.legend(handles=handles)
+        self.plot_frame.canvas.draw()
+
 
 class LogoFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        # Load image, create label widget and attach image to it
         self.codeacademy_logo = ctk.CTkImage(light_image=Image.open('codeacademy.png'),
                                              dark_image=Image.open('codeacademy.png'),
                                              size=(276, 87))
@@ -85,23 +152,44 @@ class LogoFrame(ctk.CTkFrame):
         self.label_logo = ctk.CTkLabel(self, image=self.codeacademy_logo, text='')
         self.label_logo.pack(padx=10, pady=10)
 
+
+class StatusBarFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, border_width=0,
+                         fg_color='#151515',
+                         bg_color='#151515')
+
+        font = ctk.CTkFont('Windows', size=16)
+        self.status_bar = ctk.CTkLabel(self, text='Program is ready! Enter a serial number to begin...',
+                                       text_color='darkgrey',
+                                       font=font,
+                                       pady=15,
+                                       padx=10)
+
+        self.status_bar.pack(side='left')
+
+
 class MainApp(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title("Laser data")
+        self.title("Laser Data Plotter")
         self._set_appearance_mode("dark")
-        # self.geometry("900x600")
 
-        # Initialize frames
+        # Initialize class frames
         self.stats_frame = StatsFrame(self, 'STATISTICS')
         self.plot_frame = PlotFrame(self)
         self.logo_frame = LogoFrame(self)
+        self.plot_update_frame = PlotUpdateFrame(self, self.plot_frame)
+        self.statusbar_frame = StatusBarFrame(self)
 
+        # Grid frames
         self.stats_frame.grid(row=4, column=0, rowspan=5, columnspan=2, padx=75, pady=10, sticky='nsw')
         self.plot_frame.grid(row=0, column=2, rowspan=7, columnspan=3, padx=5, pady=5, sticky='nsew')
         self.logo_frame.grid(row=0, column=0, columnspan=2)
+        self.plot_update_frame.grid(row=7, column=2, columnspan=3)
+        self.statusbar_frame.grid(row=9, column=0, columnspan=5, sticky='we')
 
-        # Creating all items
+        # Creating other UI items on the master frame
         self.label_sn = ctk.CTkLabel(self, text="Serial number:")
         self.entry_sn = ctk.CTkEntry(self, placeholder_text="K0018000")
         self.label_parameter = ctk.CTkLabel(self, text="Laser parameter:")
@@ -110,49 +198,47 @@ class MainApp(ctk.CTk):
         self.button_plot = ctk.CTkButton(self, text="Plot", command=self.plot_button)
         self.button_clear = ctk.CTkButton(self, text='Clear', command=self.clear_data_plot)
 
-        self.check_std_var = ctk.StringVar(value='off')
-        self.check_std = ctk.CTkCheckBox(self, text='Add Standard Deviation', variable=self.check_std_var, onvalue='on',
-                                         offvalue='off')
-        self.check_mean_var = ctk.StringVar(self, value='off')
-        self.check_mean = ctk.CTkCheckBox(self, text='Add Mean', variable=self.check_mean_var, onvalue='on',
-                                          offvalue='off')
-        self.button_update = ctk.CTkButton(self, text='Update plot', command=self.update_plot)
-
-        # Adding all elements with the grid, column 0
+        # Grid master frame items
         self.label_sn.grid(row=1, column=0, padx=5, pady=5)
         self.label_parameter.grid(row=2, column=0, padx=5, pady=5)
         self.button_plot.grid(row=3, column=0, padx=5, pady=5)
         self.button_clear.grid(row=3, column=1, padx=5, pady=5)
-
-        # Adding all elements with the grid, column 1
         self.entry_sn.grid(row=1, column=1, padx=5, pady=5)
         self.option_menu.grid(row=2, column=1, padx=5, pady=5)
 
-        self.check_std.grid(row=7, column=2, pady=10)
-        self.check_mean.grid(row=7, column=3, pady=10)
-        self.button_update.grid(row=7, column=4, pady=10)
-
     def plot_button(self):
-
+        # Clear canvas before drawing a new plot
         self.plot_frame.ax.clear()
         self.plot_frame.canvas.draw()
 
+        # Get values for the serial and chosen parameter
         serial = self.entry_sn.get()
         chosen_parameter = self.option_menu.get()
-        selected_laser_result = df.loc[df['Serial'] == serial, chosen_parameter].values[0]
 
-        self.stats_frame.label_selected_result.configure(text=selected_laser_result)
+        # Check if the entered serial number is in the database
+        try:
+            selected_laser_result = df.loc[df['Serial'] == serial, chosen_parameter].values[0]
+            self.stats_frame.label_selected_result.configure(text=selected_laser_result)
+            self.statusbar_frame.status_bar.configure(text=f'Data for the {serial} is ready')
+        except IndexError:
+            self.statusbar_frame.status_bar.configure(
+                text=f'{serial} is not in the database. Please enter a new serial number.')
+            self.stats_frame.label_selected_result.configure(text='Not found')
+
+        # Calculate statistics for the chosen parameter and display results in statistics frame
         self.stats_frame.label_std_result.configure(text=f'{round(df[chosen_parameter].std(), 2)}')
         self.stats_frame.label_mean_result.configure(text=f'{round(df[chosen_parameter].mean(), 2)}')
         self.stats_frame.label_max_value_result.configure(text=f'{round(df[chosen_parameter].max(), 2)}')
         self.stats_frame.label_min_value_result.configure(text=f'{round(df[chosen_parameter].min(), 2)}')
 
+        # Change size and color of the marker of the selected serial number
         size = np.where(df['Serial'] == serial, 60, 30)
         color = np.where(df['Serial'] == serial, 'tab:red', 'tab:blue')
         self.plot_frame.ax.scatter(df['Date'], df[chosen_parameter], c=color, s=size,
-                                   label=f'{chosen_parameter} of lasers')
-        self.plot_frame.ax.set_title('Selected laser comparison')
+                                   label=f'{chosen_parameter} data')
+        self.plot_frame.ax.set_title(f'Measure {chosen_parameter} of the lasers in the database')
 
+        # Create a list of possible y-axis labels (depends on a chosen parameter)
         ylabel_map = {
             'Power': 'Power, [mW]',
             'Repetition rate': 'Repetition rate, [MHz]',
@@ -161,24 +247,21 @@ class MainApp(ctk.CTk):
             'Threshold': 'Threshold, [mA]'
         }
 
-        self.plot_frame.ax.legend()
+        # Edit legend to manually add selected laser serial number, set labels for the axes
+        handles, labels = self.plot_frame.ax.get_legend_handles_labels()
+        selected_laser_point = Line2D(
+            [0], [0],
+            label=serial,
+            marker='o',
+            markersize=10,
+            markerfacecolor='r',
+            markeredgecolor='r',
+            linestyle=''
+        )
+        handles.append(selected_laser_point)
+        self.plot_frame.ax.legend(handles=handles)
         self.plot_frame.ax.set_ylabel(ylabel_map[chosen_parameter])
-        self.plot_frame.ax.set_xlabel('Measurement date')
-        self.plot_frame.canvas.draw()
-
-    def update_plot(self):
-        chosen_parameter = self.option_menu.get()
-        stdd = df[chosen_parameter].std()
-        mean = df[chosen_parameter].mean()
-
-        std_tick = self.check_std.get()
-        mean_tick = self.check_mean.get()
-
-        if std_tick == 'on':
-            self.plot_frame.ax.axhspan(mean - stdd, mean + stdd, facecolor='b', alpha=0.25, label='Standard deviation')
-        if mean_tick == 'on':
-            self.plot_frame.ax.axhline(y=mean, color='r', label='Mean')
-        self.plot_frame.ax.legend()
+        self.plot_frame.ax.set_xlabel('Date')
         self.plot_frame.canvas.draw()
 
     def clear_data_plot(self):
@@ -188,6 +271,10 @@ class MainApp(ctk.CTk):
         self.stats_frame.label_min_value_result.configure(text='0')
         self.plot_frame.ax.clear()
         self.plot_frame.canvas.draw()
+
+        self.statusbar_frame.status_bar.configure(
+            text='Plot has been cleared. Select a parameter to plot or enter a new serial number.'
+        )
 
 
 # Create and run the app
